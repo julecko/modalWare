f002ab2d17e2edbf8ca24033495da22e108266db
Julecko <julecko@dilino.sk>
2024-10-17 20:33:40 +0200

    Added bat file for compilation

---
+++ compile_library.bat
@@ -0,0 +1,17 @@
+@echo off
+
+if "%1"=="" (
+    echo File for compilation missing.
+    exit 1
+)
+set fileName=%~dp1%~n1
+
+g++ -shared -o %fileName%.dll %fileName%.def %1 -DMYLIBRARY_EXPORTS
+
+
+if %errorlevel% neq 0 (
+    echo Compilation failed.
+    exit /b %errorlevel%
+) else (
+    echo Compilation successful
+)
